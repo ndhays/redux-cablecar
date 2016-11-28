e068030d642b671b365dacc9beb855e3055eed49
Nick <ndemarestcode@gmail.com>
2016-11-27 19:51:14 -0600

    works

--- src/cableCar.js
+++ src/cableCar.js
@@ -1,30 +1,31 @@
 class CableCar {
 
-  constructor(channel) {
+  constructor(store, channel) {
 
     if (typeof ActionCable == 'undefined') {
       throw(`CableCar tried to connect to ActionCable but ActionCable is not defined`);
     }
 
     this.channel = channel;
-    this.store = null;
-    this.initialize();
+    this.store = store;
+    this.subscription = this.initialize();
   }
 
-  initialize() {
-    this.subscription = ActionCable.createConsumer().subscriptions.create({ channel: this.channel }, {
+  initialize = () => 
+    
+    ActionCable.createConsumer().subscriptions.create({ channel: this.channel }, {
       initialized: this.initialized,
       connected: this.connected,
       disconnected: this.disconnected,
       received: this.received,
       rejected: this.rejected,
-    });
-  }
+    })
   
-  dispatch = (msg) => (this.store ? this.store.dispatch(Object.assign(msg, {__ActionCable: true})) : false) 
+  // Redux dispatch function
+  dispatch = (msg) => this.store.dispatch(Object.assign(msg, {__ActionCable: true}))
 
   // ActionCable callback functions
-  initialized = () => this.dispatch({ type: 'CABLE_CAR_INITIALIZED' })
+  initialized = () => this.dispatch({ type: 'CABLE_CAR_INITIALIZED', car: this })
   
   connected = () => this.dispatch({ type: 'CABLE_CAR_CONNECTED' })
   
@@ -33,19 +34,17 @@ class CableCar {
   received = (msg) => this.dispatch(msg)
   
   rejected = (data) => {
+    this.dispatch({ type: 'CABLE_CAR_REJECTED' });
     throw(`Attempt to connect Redux store and ActionCable channel via CableCar failed. ${data}`)
   }
   
   // ActionCable subscription functions
-  perform = (action, data) => {
-    this.subscription.perform(action, data);
-  }
+  perform = (action, data) => this.subscription.perform(action, data)
   
   send = (action) => this.subscription.send(action)
   
-  unsubscribe = () => {
-    this.subscription.unsubscribe();
-  }
+  unsubscribe = () => this.subscription.unsubscribe()
+
 }
 
 
--- src/middleware.js
+++ src/middleware.js
@@ -1,27 +1,25 @@
 import CableCar from './cableCar'
 
-const middleware = (channel, s) => {
-  window.console.log(s)
-  var car = new CableCar(channel);
+const middleware = store => {
   
-  return store => next => action => {
+  var car;
+  
+  return next => action => {
 
-    if(!car.store) {
-      car.store = store;
-    }
-    
     if (action.type === 'CABLE_CAR_INITIALIZED') {
-      car.store = store;
+      car = action.car;
     } else if (action.type === 'DISCONNECT_CABLE_CAR') {
       car.unsubscribe();
     } else if (!action.__ActionCable) {
       car.send(action);
     }
-    
+  
     return (action.optimistic || action.__ActionCable ? next(action) : store.getState())
 
   }
 };
+
+middleware.connect = (store, channel) => new CableCar(store, channel)
 
 
 export default middleware
