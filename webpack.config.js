866ac78338d8ee0057712b7a18bf101e9191d71a
Nick Demarest <nick.demarest@gmail.com>
2016-11-29 19:10:03 -0600

    eslint fixes

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,13 @@ module.exports = {
     path: './build',
     filename: 'cablecar.js',
     library: 'cablecar',
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'commonjs2',
   },
   module: {
     loaders: [{
       test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-    }]
-  }
-}
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }],
+  },
+};
